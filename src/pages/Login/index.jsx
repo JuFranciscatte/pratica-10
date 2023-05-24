8988c14b91b30ba469b211e5d6d6b03052e4385a
JuFranciscatte <jufranciscatte@gmail.com>
2023-05-24 19:41:26 -0300

    add commit

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -11,6 +11,13 @@ const Login = () => {
           <FormHelperText id="login_nome_helper_text">Login.</FormHelperText>
         </FormControl>
       </Grid>
+
+    <Grid item xs={12}>
+    <FormControl fullWidth>
+      <Input id="login_nome" aria-describedby="login_nome_helper_text" value={login} onChange={e => { setLogin(e.target.value) }} />
+      <FormHelperText id="login_nome_helper_text">Login.</FormHelperText>
+    </FormControl>
+    </Grid>
     );
 }
 
